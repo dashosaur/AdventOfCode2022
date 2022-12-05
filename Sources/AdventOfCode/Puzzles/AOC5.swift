0d7c0e46ccf72cbc4b9aac2913ffa93f0b899fd4
Dash Brittain <roxanne.brittain@gmail.com>
2022-12-04 22:07:15 -0800

    AOC5: Simplify and share code

--- Sources/AdventOfCode/Puzzles/AOC5.swift
+++ Sources/AdventOfCode/Puzzles/AOC5.swift
@@ -9,61 +9,50 @@ import Foundation
 struct AOC5: Puzzle {
     typealias Answer = String
     
-    private typealias Stack = [Character]
-    
-    private func parseStacks(input: String) -> [Stack] {
-        let stackLines = input.lineGroups[0].lines.dropLast(1)
-        let stackCount = (stackLines.map(\.count).max()! + 1) / 4
-        var stacks: [Stack] = Array(repeatElement(Stack(), count: stackCount))
-        for line in stackLines.reversed() {
-            for stackIndex in 0..<stackCount {
-                let characterIndex = 1 + 4 * stackIndex
-                if characterIndex < line.count {
-                    let crate = line[characterIndex]
-                    if !crate.isWhitespace {
-                        stacks[stackIndex].append(crate)
+    private func solve(input: String, reverse: Bool) -> String {
+        
+        typealias Stack = [Character]
+        func parseStacks(input: String) -> [Stack] {
+            let stackLines = input.lineGroups[0].lines.dropLast(1)
+            let stackCount = (stackLines.map(\.count).max()! + 1) / 4
+            var stacks: [Stack] = Array(repeatElement(Stack(), count: stackCount))
+            for line in stackLines.reversed() {
+                for stackIndex in 0..<stackCount {
+                    let characterIndex = 1 + 4 * stackIndex
+                    if characterIndex < line.count {
+                        let crate = line[characterIndex]
+                        if !crate.isWhitespace {
+                            stacks[stackIndex].append(crate)
+                        }
                     }
                 }
             }
+            return stacks
         }
-        return stacks
-    }
-    
-    private struct Move {
-        let count: Int
-        let fromIndex: Int
-        let toIndex: Int
-    }
-    
-    private func parseMoves(input: String) -> [Move] {
-        input.lineGroups[1].lines.map { line in
-            let splits = line.split(separator: " ")
-            return Move(count: Int(splits[1])!,
+        
+        func parseMoves(input: String) -> [(count: Int, fromIndex: Int, toIndex: Int)] {
+            input.lineGroups[1].lines.map { line in
+                let splits = line.split(separator: " ")
+                return (count: Int(splits[1])!,
                         fromIndex: Int(splits[3])! - 1,
                         toIndex: Int(splits[5])! - 1)
-        }
-    }
-    
-    func solve1(input: String) -> String {
-        var stacks = parseStacks(input: input)
-        for move in parseMoves(input: input) {
-            for _ in 1...move.count {
-                if let crate = stacks[move.fromIndex].popLast() {
-                    stacks[move.toIndex].append(crate)
-                } else {
-                    fatalError()
-                }
             }
         }
-        return String(stacks.compactMap(\.last))
-    }
-    
-    func solve2(input: String) -> String {
+        
         var stacks = parseStacks(input: input)
         for move in parseMoves(input: input) {
-            stacks[move.toIndex].append(contentsOf: stacks[move.fromIndex].suffix(move.count))
+            let crates = stacks[move.fromIndex].suffix(move.count)
+            stacks[move.toIndex].append(contentsOf: reverse ? Array(crates.reversed()) : Array(crates))
             stacks[move.fromIndex].removeLast(move.count)
         }
         return String(stacks.compactMap(\.last))
+    }
+    
+    func solve1(input: String) -> String {
+        solve(input: input, reverse: true)
+    }
+    
+    func solve2(input: String) -> String {
+        solve(input: input, reverse: false)
     }
 }
