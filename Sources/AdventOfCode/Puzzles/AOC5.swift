713586fa3448e78ccaa884a1f9750274b360e46b
Dash Brittain <roxanne.brittain@gmail.com>
2022-12-04 22:11:58 -0800

    AOC5: More cleanup

--- Sources/AdventOfCode/Puzzles/AOC5.swift
+++ Sources/AdventOfCode/Puzzles/AOC5.swift
@@ -10,41 +10,38 @@ struct AOC5: Puzzle {
     typealias Answer = String
     
     private func solve(input: String, reverse: Bool) -> String {
-        
-        typealias Stack = [Character]
-        func parseStacks(input: String) -> [Stack] {
-            let stackLines = input.lineGroups[0].lines.dropLast(1)
-            let stackCount = (stackLines.map(\.count).max()! + 1) / 4
-            var stacks: [Stack] = Array(repeatElement(Stack(), count: stackCount))
-            for line in stackLines.reversed() {
-                for stackIndex in 0..<stackCount {
-                    let characterIndex = 1 + 4 * stackIndex
-                    if characterIndex < line.count {
-                        let crate = line[characterIndex]
-                        if !crate.isWhitespace {
-                            stacks[stackIndex].append(crate)
-                        }
+        // Parse stacks into arrays of characters that represent crates
+        let stackLines = input.lineGroups[0].lines.dropLast(1)
+        let stackCount = (stackLines.map(\.count).max()! + 1) / 4
+        var stacks: [[Character]] = Array(repeatElement([Character](), count: stackCount))
+        for line in stackLines.reversed() {
+            for stackIndex in 0..<stackCount {
+                let characterIndex = 1 + 4 * stackIndex
+                if characterIndex < line.count {
+                    let crate = line[characterIndex]
+                    if !crate.isWhitespace {
+                        stacks[stackIndex].append(crate)
                     }
                 }
             }
-            return stacks
         }
         
-        func parseMoves(input: String) -> [(count: Int, fromIndex: Int, toIndex: Int)] {
-            input.lineGroups[1].lines.map { line in
-                let splits = line.split(separator: " ")
-                return (count: Int(splits[1])!,
-                        fromIndex: Int(splits[3])! - 1,
-                        toIndex: Int(splits[5])! - 1)
-            }
+        // Parse moves into count and stack indexes
+        let moves = input.lineGroups[1].lines.map { line in
+            let splits = line.split(separator: " ")
+            return (count: Int(splits[1])!,
+                    fromIndex: Int(splits[3])! - 1,
+                    toIndex: Int(splits[5])! - 1)
         }
         
-        var stacks = parseStacks(input: input)
-        for move in parseMoves(input: input) {
+        // Execute all the moves
+        for move in moves {
             let crates = stacks[move.fromIndex].suffix(move.count)
             stacks[move.toIndex].append(contentsOf: reverse ? Array(crates.reversed()) : Array(crates))
             stacks[move.fromIndex].removeLast(move.count)
         }
+        
+        // Concatanate the top crate from each stack
         return String(stacks.compactMap(\.last))
     }
     
