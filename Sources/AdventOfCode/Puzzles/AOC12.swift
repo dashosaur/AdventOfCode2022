71448303d1dd17345241ca28cfdec2c3b1956c1d
Dash Brittain <roxanne.brittain@gmail.com>
2022-12-11 23:42:17 -0800

    AOC12: Minor cleanup

--- Sources/AdventOfCode/Puzzles/AOC12.swift
+++ Sources/AdventOfCode/Puzzles/AOC12.swift
@@ -7,26 +7,20 @@
 import Foundation
 import Algorithms
 
-fileprivate extension Character {
-    var height: Int {
-        (self == "S" ? "a" : (self == "E" ? "z" : self)).index!
-    }
-}
-
 struct AOC12: Puzzle {
     typealias Answer = Int
     
-    private func shortestPath(grid: [[Int]], startPoints: [Point], endPoint: Point) -> Int? {
-        var visitQueue = startPoints
+    private func shortestPath(map: [[Int]], startPoints: [Point], endPoint: Point) -> Int? {
+        var toVisit = startPoints
         var distanceByPoint = startPoints.reduce(into: [:]) { $0[$1] = 0 }
         
-        while let curPoint = visitQueue.first, let curDistance = distanceByPoint[curPoint] {
-            visitQueue.removeFirst()
+        while let curPoint = toVisit.first, let curDistance = distanceByPoint[curPoint], let curHeight = map[curPoint] {
+            toVisit.removeFirst()
             for nextPoint in curPoint.neighbors {
-                guard let nextHeight = grid[nextPoint], let curHeight = grid[curPoint], nextHeight <= curHeight + 1 else { continue }
+                guard let nextHeight = map[nextPoint], nextHeight <= curHeight + 1 else { continue }
                 if (curDistance + 1) < distanceByPoint[nextPoint, default: Int.max] {
                     distanceByPoint[nextPoint] = (curDistance + 1)
-                    visitQueue.append(nextPoint)
+                    toVisit.append(nextPoint)
                 }
             }
         }
@@ -34,27 +28,21 @@ struct AOC12: Puzzle {
         return distanceByPoint[endPoint]
     }
     
-    private func heightForCharacter(_ character: Character) -> Int {
-        (character == "S" ? "a" : (character == "E" ? "z" : character)).index!
-    }
-    
     private func solve(input: String, startCharacters: [Character]) -> Int {
         let lines = input.lines.map { Array($0) }
-        let grid = lines.map { $0.map(\.height) }
+        let map = lines.map { $0.map(\.height) }
         
-        var starts: [Point] = []
-        var end: Point = .origin
+        var starts = [Point]()
+        var end = Point.origin
         for point in product(0..<lines.count, 0..<lines[0].count).map({ Point($0.0, $0.1) }) {
-            switch lines[point]! {
-            case let x where startCharacters.contains(x):
+            guard let character = lines[point] else { continue }
+            if startCharacters.contains(character) {
                 starts.append(point)
-            case "E":
+            } else if character == "E" {
                 end = point
-            default:
-                break
             }
         }
-        return shortestPath(grid: grid, startPoints: starts, endPoint: end)!
+        return shortestPath(map: map, startPoints: starts, endPoint: end)!
     }
     
     func solve1(input: String) -> Int {
@@ -63,5 +51,11 @@ struct AOC12: Puzzle {
     
     func solve2(input: String) -> Int {
         solve(input: input, startCharacters: ["S", "a"])
+    }
+}
+
+fileprivate extension Character {
+    var height: Int {
+        (self == "S" ? "a" : (self == "E" ? "z" : self)).index!
     }
 }
